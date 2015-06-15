5ff8d5a53a07d5354def76d012974635ee06de51
Luciano Porto <luciano.bapo@gmail.com>
2015-06-15 06:51:00 -0300

    acertos

--- resources/views/delivery/partials/footer.blade.php
+++ resources/views/delivery/partials/footer.blade.php
@@ -12,9 +12,22 @@
         $('[data-toggle="popover"]').popover();
     });
 
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    ga('create', '".getenv("GOOGLE_ANALYTICS_ID")."', 'auto');
+//    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+//    ga('create', '".getenv("GOOGLE_ANALYTICS_ID")."', 'auto');
+//    ga('require', 'linkid', 'linkid.js');
+//    ga('require', 'displayfeatures');
+//    ga('send', 'pageview');
+</script>
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-59766919-1', 'auto');
     ga('require', 'linkid', 'linkid.js');
     ga('require', 'displayfeatures');
     ga('send', 'pageview');
+
 </script>
