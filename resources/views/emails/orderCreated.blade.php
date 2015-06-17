6a5b33a2f3fa128ccd627fb7db7af4db06a58685
Luciano Porto <luciano.bapo@gmail.com>
2015-06-17 00:38:47 -0300

    acerto delivery

--- resources/views/emails/orderCreated.blade.php
+++ resources/views/emails/orderCreated.blade.php
@@ -1,7 +1,7 @@
 <div style="padding: 19px;margin-bottom: 20px;
     background-color: #f5f5f5;border: 1px solid #e3e3e3;border-radius: 4px;
     box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.05);">
-    <h1>Nova Ordem nº {!! link_to_route('orders.edit',$order->id,[$host,$order->id]) !!}</h1>
+    <h1>Nova Ordem nº {!! link_to_route('orders.edit',$order->id,['laravel',$order->id]) !!}</h1>
     <p>Entregar em: {{ $order->address->endereco }}</p>
     <p>Tipo: {{ $order->type->descricao }}</p>
     <p>Pagamento: {{ $order->payment->descricao }}</p>
