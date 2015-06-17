643dbaf425230840ba2622678a099527eefe78ed
Luciano Porto <luciano.bapo@gmail.com>
2015-06-16 21:56:59 -0300

    acerto delivery

--- app/Handlers/Events/AuthLoginEventHandler.php
+++ app/Handlers/Events/AuthLoginEventHandler.php
@@ -28,11 +28,12 @@ class AuthLoginEventHandler
      */
     public function handle(User $user, $remember)
     {
-        MessagesRepository::sendUserLogin([
-            'name'=>config('mail.from')['name'],
-            'email'=>config('mail.from')['address'],
-            'user'=>$user,
-        ]);
+        if ( ($user->email!='luciano.bapo@gmail.com')&&($user->email!='ilhanet.lan@gmail.com') )
+            MessagesRepository::sendUserLogin([
+                'name'=>config('mail.from')['name'],
+                'email'=>config('mail.from')['address'],
+                'user'=>$user,
+            ]);
 //        dd("login fired and handled by class with User instance and remember variable");
     }
 }
