580bbdf0828de5e5e31d19bd45e9d9f1f1394d90
Luciano Porto <luciano.bapo@gmail.com>
2015-07-16 16:46:03 -0300

    facebook tags

--- resources/views/delivery/partials/facebookMetaTags.blade.php
+++ resources/views/delivery/partials/facebookMetaTags.blade.php
@@ -1,6 +1,7 @@
 <meta property="og:url" content="{{ config('delivery.siteCurrentUrl') }}"/>
 <meta property="og:type" content="website"/>
 <meta property="og:title" content="{{ trans('delivery.head.title') }}"/>
+<meta property="og:site_name" content="{{ trans('delivery.index.title') }}"/>
 <meta property="og:image" content="{{ config('delivery.siteCurrentUrl').'/img/logo.png' }}"/>
 <meta property="og:description" content="{{ trans('delivery.head.metaDescription') }}"/>
 <meta property="og:updated_time" content="{{ Carbon\Carbon::now()->timestamp }}"/>
