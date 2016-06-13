47fe09ab1dfac08186f6f6c44d5ffc8c9a6ce078
Luciano Porto <luciano.bapo@gmail.com>
2016-06-12 21:21:36 -0300

    facebook meta tags

--- resources/views/delivery/partials/facebookMetaTags.blade.php
+++ resources/views/delivery/partials/facebookMetaTags.blade.php
@@ -1,8 +1,14 @@
 <meta property="fb:app_id" content="{{ config('services.facebook.client_id') }}" />
 <meta property="fb:admins" content="riodasostrasdelivery24hs"/>
 
+<meta name="twitter:card" content="photo"/>
+<meta name="twitter:url" content="{{ config('delivery.siteMetaTagCurrentUrl') }}"/>
+<meta name="twitter:title" content="{{ trans('delivery.head.title') }}"/>
+<meta name="twitter:description" content="{{ trans('delivery.head.metaDescription') }}"/>
+<meta name="twitter:image" content="{{ config('delivery.siteImage') }}"/>
+
 <meta property="og:url" content="{{ config('delivery.siteMetaTagCurrentUrl') }}"/>
-<meta property="og:type" content="website"/>
+<meta property="og:type" content="bar"/>
 <meta property="og:title" content="{{ trans('delivery.head.title') }}"/>
 <meta property="og:site_name" content="{{ trans('delivery.index.title') }}"/>
 <meta property="og:image" content="{{ config('delivery.siteImage') }}"/>
