3896f837c24e7ab642924b24c4d7a6205b4f0157
Luciano Porto <luciano.bapo@gmail.com>
2016-08-28 11:44:06 -0300

    redirect

--- app/Http/Middleware/AfterMiddleware.php
+++ app/Http/Middleware/AfterMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Models\Traffic;
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class AfterMiddleware
 {
@@ -13,7 +14,8 @@ class AfterMiddleware
         $response = $next($request);
 
         // Perform action
-
+        if (strpos(url(), 'delivery.')!==false && env('APP_ENV')=='production')
+            return Redirect::to('https://delivery24horas.com', 301);
         return $response;
     }
 }
